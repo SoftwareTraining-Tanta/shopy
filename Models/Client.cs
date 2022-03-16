b9f1af818b73b6f9f0990bbadbc493b80ede27ec
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-03-16 20:38:14 +0200

    Update Client.cs

--- Models/Client.cs
+++ Models/Client.cs
@@ -1,9 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
-
+using System.Linq;
 #nullable disable
 
 namespace Models
@@ -38,10 +37,113 @@ namespace Models
         [Column("email")]
         [StringLength(60)]
         public string Email { get; set; }
+        [Required]
+        [Column("password")]
+        [StringLength(100)]
+        public string Password { get; set; }
 
         [InverseProperty(nameof(Cart.Client))]
         public virtual ICollection<Cart> Carts { get; set; }
         [InverseProperty(nameof(Product.Client))]
         public virtual ICollection<Product> Products { get; set; }
+        public enum Properities { Name, Id, Email, Phone, City, Country, Password };
+        public static string Add(Client client)
+        {
+
+            using (Shopy db = new())
+            {
+                try
+                {
+                    db.Clients.Add(client);
+                    db.SaveChanges();
+                    return "Done adding client";
+                }
+                catch
+                {
+                    return "Client is already inserted";
+                }
+            }
+        }
+        public static dynamic Get(int id)
+        {
+            bool isFound = Check(id);
+            using (Shopy db = new())
+            {
+                try
+                {
+                    if (isFound)
+                    {
+                        Client client = db.Clients.Where(client => client.Id == id).First();
+                        return client;
+                    }
+                    return MyExceptions.ClientNotFound(id);
+                }
+                catch (Exception ex)
+                {
+                    return ex.Message;
+                }
+            }
+
+        }
+        private static bool Check(int id)
+        {
+            using (Shopy db = new())
+            {
+                try
+                {
+                    Client client = db.Clients.Where(client => client.Id == id).First();
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+        public static dynamic Delete(int id)
+        {
+            bool isFound = Check(id);
+            using (Shopy db = new())
+            {
+                if (isFound)
+                {
+                    Client client = db.Clients.Where(client => client.Id == id).First();
+                    db.Clients.Remove(client);
+                    db.SaveChanges();
+                    return client;
+                }
+                return MyExceptions.ClientNotFound(id);
+            }
+        }
+        public static void Update(int id, dynamic value, Properities properities = Properities.Name)
+        {
+
+            using (Shopy db = new())
+            {
+                Client client = db.Clients.Where(c => c.Id == id).First();
+                switch (properities)
+                {
+                    case Properities.Name:
+                        client.Name = value;
+                        break;
+                    case Properities.Country:
+                        client.Country = value;
+                        break;
+                    case Properities.City:
+                        client.City = value;
+                        break;
+                    case Properities.Email:
+                        client.Email = value;
+                        break;
+                    case Properities.Phone:
+                        client.Phone = value;
+                        break;
+                    default:
+                        client.Name = value;
+                        break;
+                }
+                db.SaveChanges();
+            }
+        }
     }
 }
