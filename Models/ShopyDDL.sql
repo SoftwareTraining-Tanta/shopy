37eda57b786f23777f03dab4264827ac9e19d436
Mohamed Harby <71990569+Mohamed-Harby@users.noreply.github.com>
2022-03-17 20:57:49 +0200

    Update ShopyDDL.sql

--- Models/ShopyDDL.sql
+++ Models/ShopyDDL.sql
@@ -1,17 +1,20 @@
 create database shopy;
 use shopy;
 
+drop database shopy;
+
 create table vendors
 (
     id       int auto_increment,
     name     varchar(30)  not null,
     country  varchar(30),
     city     varchar(30),
-    phone    varchar(20)  not null,
-    email    varchar(60)  not null,
+    phone    varchar(20)  not null unique ,
+    email    varchar(60)  not null unique ,
     password varchar(100) not null,
     primary key (id)
 );
+
 
 create table clients
 (
@@ -19,8 +22,8 @@ create table clients
     name     varchar(30)  not null,
     country  varchar(30),
     city     varchar(30),
-    phone    varchar(20)  not null,
-    email    varchar(60)  not null,
+    phone    varchar(20)  not null unique ,
+    email    varchar(60)  not null unique ,
     password varchar(100) not null,
     primary key (id)
 );
@@ -34,14 +37,15 @@ create table carts
     phone    varchar(20) not null,
     email    varchar(60) not null,
     primary key (id),
-    foreign key (clientId) references clients (id)
+    foreign key (clientId) references clients (id) on delete cascade
 );
+
 
 create table products
 (
     id        int auto_increment,
     vendorId  int           not null,
-    clientId  int           not null,
+    clientId  int,
     category  varchar(20)   not null check (category in
                                             ('Ram', 'Monitor', 'SSD', 'HDD', 'K&M', 'Laptop', 'FlashDrive')),
     model     varchar(30),
@@ -54,4 +58,3 @@ create table products
     foreign key (clientId) references clients (id),
     foreign key (cartId) references carts (id)
 );
-
