0a7cda101144b664227ec79e8ad8d2fb090ee9f6
Mohamed Harby <71990569+Mohamed-Harby@users.noreply.github.com>
2022-03-18 19:13:40 +0200

    Update MyExceptions.cs

--- Models/MyExceptions.cs
+++ Models/MyExceptions.cs
@@ -1,12 +1,11 @@
 using System;
-namespace Shopy.Models;
+namespace Shopy.Models.shared;
 
 public class MyExceptions
 {
     public static string ClientNotFound(int id) => $"Client with id : {id} is not found";
     public static string VendortNotFound(int id) => $"Vendor with id : {id} is not found";
     public static string CartNotFound(int id) => $"Cart with id : {id} is not found";
-    public static string InvalidDeletion(int id) => $"Can't Delete This Client";
     public static string ProductNotFound(int id) => $"Product with id : {id} is not found";
     public static string PhoneNumberIsUsed(string phone) => $"Phone number : {phone} is used before";
     public static string EmailIsUsed(string email) => $"Email : {email} is already used";
