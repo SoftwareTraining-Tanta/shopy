ab4c02da6768cc0c40f42506fe88f6fe0d70d45f
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-03-21 14:45:40 +0200

    Add files via upload

--- Models/Client.cs
+++ Models/Client.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.Extensions.Logging;
-using Shopy.Models.shared;
+using Shopy.Models.Dtos;
+using Shopy.Models.Shared;
 #nullable disable
 
 namespace Shopy.Models
@@ -54,13 +50,25 @@ namespace Shopy.Models
 
 
         public enum Properties { Name, Email, Phone, City, Country, Password };
-        public static string Add(Client client)
+        public static string Add(ClientDto clientDto)
         {
             using (ShopyCtx db = new())
             {
                 // ILoggerFactory loggerFactory = db.GetService<ILoggerFactory>();
                 // loggerFactory.AddProvider(new ConsoleLoggerProvider());
 
+                Client client = new Client
+                {
+                    Id = clientDto.Id,
+                    City = clientDto.City,
+                    Country = clientDto.Country,
+                    Email = clientDto.Email,
+                    Name = clientDto.Name,
+                    Password = clientDto.Password,
+                    Phone = clientDto.Phone,
+                    Carts = new List<Cart>(),
+                    Products = new List<Product>(),
+                };
                 var getClient = db.Clients.FirstOrDefault(cl => cl.Equals(client));
                 if (getClient != null)
                 {
@@ -69,7 +77,7 @@ namespace Shopy.Models
 
                 db.Clients.Add(client);
                 client.Carts.Add(
-                    new Cart()
+                    new Cart
                     {
                         ClientId = client.Id,
                         Country = client.Country,
@@ -82,12 +90,12 @@ namespace Shopy.Models
                 return "Done adding client";
             }
         }
-        public static dynamic Get(int id)
+        public static Client Get(int id)
         {
             bool isFound = Exist(id);
             if (!isFound)
             {
-                return MyExceptions.ClientNotFound(id);
+                return new();
             }
             using (ShopyCtx db = new())
             {
@@ -107,9 +115,17 @@ namespace Shopy.Models
                 return MyExceptions.ClientNotFound(id);
             using ShopyCtx db = new();
             var client = db.Clients.FirstOrDefault(client => client.Id == id);
+            var cart = Cart.Get(id);
+            var products = Cart.InCart(id);
             // Cart cart = db.Carts.Where(c => c.ClientId == Id).FirstOrDefault();
             // Cart.Delete(cart);
-            if (client != null) db.Clients.Remove(client);
+            if (client != null)
+            {
+                foreach (Product p in products)
+                    db.Products.Remove(p);
+                db.Carts.Remove(cart);
+                db.Clients.Remove(client);
+            }
             db.SaveChanges();
             return "Client Deleted";
         }
@@ -123,41 +139,41 @@ namespace Shopy.Models
                 switch (Properity)
                 {
                     case Properties.Name:
-                        if(client != null) client.Name = value;
+                        if (client != null) client.Name = value;
                         break;
                     case Properties.Country:
-                        if(client != null) client.Country = value;
+                        if (client != null) client.Country = value;
                         break;
                     case Properties.City:
-                        if(client != null) client.City = value;
+                        if (client != null) client.City = value;
                         break;
                     case Properties.Email:
-                        if(client != null) client.Email = value;
+                        if (client != null) client.Email = value;
                         break;
                     case Properties.Phone:
-                        if(client != null) client.Phone = value;
+                        if (client != null) client.Phone = value;
                         break;
                     case Properties.Password:
-                        if(client != null) client.Password = value;
+                        if (client != null) client.Password = value;
                         break;
                     default:
-                        if(client != null) client.Name = value;
+                        if (client != null) client.Name = value;
                         break;
                 }
                 db.SaveChanges();
                 return "Updated";
             }
         }
-        public static List<Client> AllClients()
+        public static List<ClientDto> AllClients(int limit)
         {
             using (ShopyCtx db = new())
             {
-                var clients = db.Clients.DefaultIfEmpty().ToList();
-                if(clients != null) return clients;
-                return new List<Client>();
+                List<Client> clients = db.Clients.Take(limit).DefaultIfEmpty().ToList();
+                if (clients != null) return clients.AsDto();
+                return new List<ClientDto>();
             }
         }
-        public static List<Product> ClientProducts(int ClientId) // previously bought products
+        public static List<ProductDto> ClientProducts(int ClientId) // previously bought products
         {
             using (ShopyCtx db = new())
             {
@@ -165,10 +181,10 @@ namespace Shopy.Models
                 // loggerFactory.AddProvider(new ConsoleLoggerProvider());
 
                 // if the product is in the cart, It will not be in this list.
-        
+
                 var products = db.Products.Where(p => p.ClientId == ClientId && p.CartId == null).ToList();
-                if (products != null) return products;
-                return new List<Product>();
+                if (products != null) return products.AsDto().ToList();
+                return new List<ProductDto>();
                 // return client.Products.Where(p => p.CartId == null).ToList(); // use join
 
             }
