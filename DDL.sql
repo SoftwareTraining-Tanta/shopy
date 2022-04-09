e0fb610d59dbfb575cf930abf5c071025e143baf
Mohamed Harby <71990569+Mohamed-Harby@users.noreply.github.com>
2022-04-09 12:35:39 +0200

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -1,3 +1,4 @@
+drop database if exists shopy;
 create database shopy;
 use shopy;
 
@@ -7,8 +8,8 @@ create table vendors
     name     varchar(30)  not null,
     country  varchar(30),
     city     varchar(30),
-    phone    varchar(20)  not null,
-    email    varchar(60)  not null,
+    phone    varchar(20)  not null unique,
+    email    varchar(60)  not null unique,
     password varchar(100) not null,
     primary key (id)
 );
@@ -19,8 +20,8 @@ create table clients
     name     varchar(30)  not null,
     country  varchar(30),
     city     varchar(30),
-    phone    varchar(20)  not null,
-    email    varchar(60)  not null,
+    phone    varchar(20)  not null unique,
+    email    varchar(60)  not null unique,
     password varchar(100) not null,
     primary key (id)
 );
@@ -37,21 +38,30 @@ create table carts
     foreign key (clientId) references clients (id) on delete cascade
 );
 
+create table models
+(
+    name      varchar(300) primary key,
+    imagePath varchar(512)  not null,
+    price     numeric(9, 2) not null,
+    salePrice decimal default null,
+    brand     varchar(100),
+    color     varchar(50),
+    features  varchar(600)
+);
+
 create table products
 (
-    id        int auto_increment,
-    vendorId  int           not null,
-    clientId  int
-    category  varchar(20)   not null check (category in
-                                            ('Ram', 'Monitor', 'SSD', 'HDD', 'K&M', 'Laptop', 'FlashDrive')),
-    model     varchar(30),
-    price     numeric(7, 2) not null,
-    details   varchar(512),
-    imagePath varchar(512),
-    cartId    int,
+    id       int auto_increment,
+    vendorId int          not null,
+    category varchar(20)  not null check (category in
+                                          ('RAM', 'SSD', 'K&M', 'Mouse')),
+    model    varchar(300) not null,
+    clientId int,
+    cartId   int,
+    rate     numeric(2, 1) default 0 check (rate between 0.0 and 5.0),
     primary key (id),
     foreign key (vendorId) references vendors (id),
     foreign key (clientId) references clients (id),
-    foreign key (cartId) references carts (id)
+    foreign key (cartId) references carts (id),
+    foreign key (model) references models (name)
 );
-
