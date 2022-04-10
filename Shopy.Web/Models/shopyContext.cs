ec16034f72ba53adcb0fda7b1a0f98e5c4550db3
ahmedyounis253 <amy2532001@gmail.com>
2022-04-10 21:38:29 +0200

    handling databases managing

--- Shopy.Web/Models/shopyContext.cs
+++ Shopy.Web/Models/shopyContext.cs
@@ -28,7 +28,8 @@ namespace Shopy.Web.Models
             if (!optionsBuilder.IsConfigured)
             {
                 // #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySQL("server=localhost;database=shopy;username=root;password=2510203121");
+                optionsBuilder.UseMySql("server = localhost; database = shopy; username = root; password =test;",
+new MySqlServerVersion(new Version(10, 4, 17)));
             }
         }
 
