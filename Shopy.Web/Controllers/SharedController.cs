4de2a7b75b0bfc4d3a9281fec2c376df6163453d
Mohammad Ahmed Badee <mohammed30981710@f-eng.tanta.edu.eg>
2022-04-18 03:42:47 +0200

    Add files via upload

---
+++ Shopy.Web/Controllers/SharedController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Shopy.Web.Shared;
+[ApiController]
+[Route("api/shared/")]
+public class SharedController : ControllerBase
+{
+    [HttpPost("ContactUs/{name}/{email}/{message}")]
+    public ActionResult ContactUs(string name, string email, string message)
+    {
+        try
+        {
+            Smtp.SendMessage(email, "from : " + email, name + "\n" + message);
+            return Ok("Message Sent");
+        }
+        catch (Exception ex)
+        {
+            return BadRequest("Failed to send message : " + ex.Message);
+        }
+    }
+
+}
